5a221c3006fa65cb929321dc4c10a88758428eb5
Daniel Bacon <dbacon338@gmail.com>
2017-04-21 21:45:01 +0000

    add methods

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,3 +17,25 @@ from os import getenv
 app = Flask(__name__)
 CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
+
+@app.errorhandler(404)
+def not_found(error):
+    '''This is the 'not_found' method.
+    Creates JSON 404 page.
+    '''
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    '''This is the 'teardown' method.
+    Closes a given session.
+    '''
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    print(app.url_map)
+    app.run(host=host, port=port)
