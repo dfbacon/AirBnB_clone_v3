a747346c86d9ecb3f42637d73bb4e703f61a1e4f
Daniel Bacon <dbacon338@gmail.com>
2017-04-23 22:23:27 +0000

    update methods

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -15,7 +15,6 @@ from os import getenv
 from sqlalchemy import inspect
 
 if getenv('HBNB_TYPE_STORAGE', 'fs') != 'db':
-
     @app_views.route('/places/<place_id>/amenities/', methods=['GET'])
     def view_place_amenity(place_id):
         '''This is the 'view_place_amenity' method.
@@ -37,6 +36,18 @@ if getenv('HBNB_TYPE_STORAGE', 'fs') != 'db':
 
         Deletes an Amenity object of a given place.
         '''
+        place = storage.get("Place", place_id)
+        if place is None:
+            abort(404)
+
+        if amenity_id is not None:
+            for instance in range(len(place.amenities)):
+                if place.amenity[instance] == amenity_id:
+                    place.amenity.pop(instance)
+                    place.save()
+
+        return jsonify({}), 200
+
 
     @app_views.route('/places/<place_id>/amenities/<amenity_id>/',
                      methods=['POST'])
@@ -45,6 +56,20 @@ if getenv('HBNB_TYPE_STORAGE', 'fs') != 'db':
 
         Creates a link between an Amenity object and a given place.
         '''
+        place = storage.get("Place", place_id)
+        if place is None:
+            abort(404)
+
+        amenity = storage.get("Amenity", amenity_id)
+        if amenity is None:
+            return "Bad amenity", 404
+
+        if amenity_id in [instance for instance in place.amenities]:
+            return jsonify(amenity.to_json()), 200
+
+        place.amenities.append(amenity_id)
+        place.save()
+        return jsonify(amenity.to_json()), 201
 
 else:
     @app_views.route('/places/<place_id>/amenities/', methods=['GET'])
@@ -68,6 +93,18 @@ else:
 
         Deletes an Amenity object of a place.
         '''
+        place = storage.get("Place", place_id)
+        if place is None:
+            abort(404)
+
+        amenity = storage.get("Amenity", amenity_id)
+        if amenity is not None:
+            place.amenities.remove(amenity)
+            place.save()
+            return jsonify(place.amenities), 200
+
+        return jsonify({}), 200
+
 
     @app_views.route('/places/<place_id>/amenities/<amenity_id>/',
                      methods=['POST'])
@@ -76,3 +113,18 @@ else:
 
         Creates a link between an Amenity object and a given place.
         '''
+        place = storage.get("Place", place_id)
+        if place is None:
+            return "Bad place", 404
+
+        amenity = storage.get("Amenity", amenity_id)
+        if amenity is None:
+            return "Bad amenity", 404
+
+        if amenity in place.amenities:
+            return jsonify(amenity.to_json()), 200
+
+        place.amenities.append(amenity)
+        place.save()
+        storage.save()
+        return jsonify(amenity.to_json()), 201
