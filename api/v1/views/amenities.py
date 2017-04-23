2d31eb16db45d8a46a264ebdfd95915110626602
Daniel Bacon <dbacon338@gmail.com>
2017-04-23 21:10:23 +0000

    update all amenity methods

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -18,6 +18,17 @@ def view_amenity(amenity_id=None):
 
     Retrieves a given Amenity object.
     '''
+    if amenity_id is None:
+        amenities = [
+            state.to_json() for state in storage.all("Amenity").values()]
+        return jsonify(amenities)
+
+    amenity = storage.get("Amenity", amenity_id)
+    if amenity is None:
+        abort(404)
+
+    return jsonify(amenity.to_json())
+
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
 def delete_amenity(amenity_id=None):
@@ -25,6 +36,13 @@ def delete_amenity(amenity_id=None):
 
     Deletes a given Amenity object.
     '''
+    amenity = storage.get("Amenity", amenity_id)
+    if amenity is None:
+        abort(404)
+
+    storage.delete(amenity)
+    return jsonify({}), 200
+
 
 @app_views.route('/amenities/', methods=['POST'])
 def create_amenity():
@@ -32,6 +50,18 @@ def create_amenity():
 
     Creates a new Amenity object.
     '''
+    try:
+        r = request.get_json()
+        if 'name' not in r.keys():
+            return "Missing name", 400
+
+        amenity = Amenity(**r)
+        amenity.save()
+        return jsonify(amenity.to_json()), 201
+
+    except:
+        return "Not a JSON", 400
+
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'])
 def update_amenity(amenity_id=None):
@@ -39,3 +69,20 @@ def update_amenity(amenity_id=None):
 
     Updates a given Amenity object.
     '''
+    try:
+        r = request.get_json()
+        amenity = storage.get("Amenity", amenity_id)
+        if amenity is None:
+            abort(404)
+
+        for instance in ("id", "created_at", "updated_at"):
+            r.pop(instance, None)
+
+        for key, value in r.items():
+            setattr(amenity, key, value)
+
+        amenity.save()
+        return jsonify(amenity.to_json()), 200
+
+    except:
+        return "Not a JSON", 400
