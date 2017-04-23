d4e0ac8857f097f048b7fd3f9cbf1c0847f68d00
Daniel Bacon <dbacon338@gmail.com>
2017-04-23 20:15:26 +0000

    reviews framework

---
+++ api/v1/views/reviews.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+'''
+
+This is the 'reviews' module.
+
+reviews is a new view for Review objects.
+Handles all default RestFul API actions.
+
+'''
+from api.v1.views import (app_views, Review, storage)
+from flask import (abort, jsonify, request)
+
+
+@app_views.route("/places/<place_id>/reviews", methods=["GET"])
+def all_reviews(place_id):
+    '''This is the 'all_reviews' method.
+
+    Compiles a list of all Review objects of a given place.
+    '''
+
+@app_views.route("/reviews/<review_id>", methods=["GET"])
+def single_review(review_id):
+    '''This is the 'single_review' method.
+
+    Returns a single Review object of a given place.
+    '''
+
+@app_views.route("/reviews/<review_id>", methods=["DELETE"])
+def delete_single_review(review_id):
+    '''This is the 'delete_single_review' method.
+
+    Deletes a single Review object.
+    '''
+
+@app_views.route("/places/<place_id>/reviews", methods=["POST"])
+def create_review(place_id):
+    '''This is the 'create_review' method.
+
+    Creates a Review object of a given place.
+    '''
+
+@app_views.route("/reviews/<review_id>", methods=["PUT"])
+def update_review(review_id):
+    '''This is the 'update_review' method.
+
+    Updates a Review object.
+    '''
