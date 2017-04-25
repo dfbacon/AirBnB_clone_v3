76b37474128ee2af460334d645d0d8c80a76aa8a
Daniel Bacon <dbacon338@gmail.com>
2017-04-25 22:37:37 +0000

    update testing suite

--- tests/test_api/test_v1/test_app.py
+++ tests/test_api/test_v1/test_app.py
@@ -6,27 +6,36 @@ This is the 'test_app' module.
 test_app is the testing suite for the 'app' module.
 
 '''
-import flask
-print("HERE")
-print("HERE")
-import unittest
-print("HERE")
-from models import storage
-print("HERE")
 from api.v1.app import app
-print("HERE")
+import flask
+import json
+from models import storage
+import unittest
 
 
 class TestApp(unittest.TestCase):
+    '''This is the 'TestApp' class.
 
-    def setUp(self):
+    Testing suite for the 'app' module.
+    '''
+    @classmethod
+    def setUpClass(self):
+        '''This is the 'setUpClass' method.
+
+        Initializes the testing suite.
+        '''
         app.config['TESTING'] = True
         self.app = app.test_client()
 
     def test_404(self):
+        '''This is the 'test_404' method.
+
+        Tests the 'not_found' method.
+        '''
         rv = self.app.get('/bad')
         self.assertEqual(rv.status_code, 404)
         self.assertEqual(rv.headers.get("Content-Type"), "application/json")
+
         json_format = json.loads(str(rv.get_data(), encoding="utf-8"))
         self.assertEqual(json_format.get("error"), "Not found")
 
