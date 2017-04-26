983dab8c5cd698569c1d8e7035c2239bda3ba513
Daniel Bacon <dbacon338@gmail.com>
2017-04-26 05:44:02 +0000

    reorder

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.index import *
-from api.v1.views.places import *
-from api.v1.views.places_amenities import *
-from api.v1.views.places_reviews import *
-from api.v1.views.states import *
-from api.v1.views.users import *
 from flask import Blueprint
 from models import storage
 from models.amenity import Amenity
@@ -16,6 +8,14 @@ from models.review import Review
 from models.state import State
 from models.user import User
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.index import *
+from api.v1.views.places import *
+from api.v1.views.places_amenities import *
+from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
 '''
 
 This is the initialization module.
