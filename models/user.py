cd7836646b40e2b18f453b28f2af49027d12625a
Daniel Bacon <dbacon338@gmail.com>
2017-04-28 17:40:52 +0000

    update password as protected attribute

--- models/user.py
+++ models/user.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel, Base, Table, Column, String
-from sqlalchemy.orm import relationship, backref
-from os import getenv
 """
 user module
     contains
         The User Class inherts from BaseModel, Base
 """
+from models.base_model import BaseModel, Base, Table, Column, String
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
 class User(BaseModel, Base):
@@ -16,9 +16,11 @@ class User(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = "users"
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        _password = Column("password", String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete, delete-orphan")
         places = relationship("Place", backref="user",
                               cascade="all, delete, delete-orphan")
     else:
@@ -31,4 +33,20 @@ class User(BaseModel, Base):
         """
         initializes from BaseModel
         """
+        value = kwargs.get("password", "")
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        return self.__dict__.get('_password', "")
+
+    @password.setter
+    def password(self, value):
+        """
+        hash the password
+
+        Argument:
+           value: password new value
+        """
+        pwd = bytes(value.encode("utf-8"))
+        self.__dict__['_password'] = hashlib.md5(pwd).hexdigest()
