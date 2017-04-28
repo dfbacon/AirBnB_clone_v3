fcb41108833188634130d64db80ca2a7e5fd9311
Daniel Bacon <dbacon338@gmail.com>
2017-04-28 17:32:07 +0000

    update from/import

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel, Base, Table, Column, String
 from os import getenv
+from sqlalchemy.orm import relationship, backref
 """
 amenity module
     contains
